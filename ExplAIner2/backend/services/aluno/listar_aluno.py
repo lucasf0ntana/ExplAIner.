c8eb27cdc6809b8a5625fb76056b83f4cf0878dc
lucasf0ntana <lucasfontana850@gmail.com>
2026-07-02 09:57:51 -0300

    Delete ExplAIner2 directory

--- ExplAIner2/backend/services/aluno/listar_aluno.py
+++
@@ -1,8 +0,0 @@
-from backend.models.model_aluno import Aluno
-
-class ListarAlunosService:
-
-    def executar(self):
-        alunos = Aluno.query.all()
-        
-        return [aluno.to_dict() for aluno in alunos]
